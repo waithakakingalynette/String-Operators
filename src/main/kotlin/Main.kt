f4caceaea47e8f8759dc47a7bc4cd3730066a970
waithakakingalynette <warukiralynette@gmail.com>
2023-02-17 11:09:55 +0300

    string operators

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,40 @@
+fun main (){
+    val string="akirachix"
+    println("akirachix"[0])
+    println("akirachix"[1])
+    println("akirachix"[3])
+    val stmt=("x" + "name"+ "y")
+    println("my name is"+ " " + "Lynette"+ " " + "and I am 19 years old" )
+    val age= 19
+    println("my age is " + 19.toString())
+    val name= "Lynette"
+    val name2= "Grace"
+    if (name==(name2)){
+        println("I know you")}else{
+        println("I do not know who that is")}
+}
+
+
+//1.Given a string “akirachix”. Write a function that prints out a string composed
+//of the first, third and fourth characters of the string (2 points)
+
+fun indexing(){
+    val string=("String")
+}
+
+//2. Write a function that takes in 2 parameters, name and age and returns a
+//String with this structure “Hi, my name is x and I am y years old.” Where x and
+//y are the provided name and age respectively. (3 points)
+fun concatenation(){
+    val x= "my name is "
+    val name= "Lynette"
+    val y= " and I am 19 years old"
+}
+
+
+//3. Write a function that takes in a String and returns its length (2 points)
+fun string(){
+    val age= 19
+
+}
+
